319098d32813c807308ef1564ea5d5cced832bfc
Michael Kamau <kamau.karitu@gmail.com>
2022-05-05 20:42:37 +0300

    Least number greater than one not in array

---
+++ python/SmallestArrayInteger.py
@@ -0,0 +1,23 @@
+"""
+Write a function:
+def solution(A)
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+Given A = [1, 2, 3], the function should return 4.
+Given A = [−1, −3], the function should return 1.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+5:26
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+"""
+
+def solution(A):
+    if(max(A) < 0 ):
+        return 1
+    else:
+        for no in range (1, max(A)+2):
+            if no not in A:
+                return no
